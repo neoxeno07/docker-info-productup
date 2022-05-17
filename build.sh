70fb6dde86b3c7ee4fd00063748918d2595f5636
Arun Abraham <arunnabraham@gmail.com>
2022-05-17 14:10:25 +0530

    changes

--- build.sh
+++ build.sh
@@ -5,5 +5,17 @@ if [ "$(ls -A $(pwd)/app/productup-challenge-xml-data-importer)" ]
  else
     git clone git@github.com:arunnabraham/productup-challenge-xml-data-importer.git
 fi
-podman build --no-cache -t localhost/php-cli-alpine:latest .
+docker build --no-cache -t localhost/php-cli-alpine:latest .
 rm -rf ./productup-challenge-xml-data-importer
+
+
+
+## if [("$CONTAINER_TECHNOLOGY"!="docker")]
+# then
+#    podman run \
+#    -v $(pwd)/.env:$CONTAINER_WORK_DIR'/.env':z \
+#    -v $(pwd)/logs:$CONTAINER_WORK_DIR'/logs':z \
+#    -v $(pwd)/outputfiles:$CONTAINER_WORK_DIR'/outputfiles':z \
+#    -v $(pwd)/tmp:/tmp:z \
+#    -i --rm localhost/php-cli-alpine:latest php ./run $1 $2 $3 $4 $5 $6
+## else
--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 if [ -f .env ]; then
-  export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
+    export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
 
-podman run \
--v $(pwd)/.env:$CONTAINER_WORK_DIR'/.env':z \
--v $(pwd)/logs:$CONTAINER_WORK_DIR'/logs':z \
--v $(pwd)/outputfiles:$CONTAINER_WORK_DIR'/outputfiles':z \
--v $(pwd)/tmp:/tmp:z \
--i --rm localhost/php-cli-alpine:latest php ./run $1 $2 $3 $4 $5 $6
+    docker run \
+    -v $(pwd)/.env:$CONTAINER_WORK_DIR'/.env' \
+    -v $(pwd)/logs:$CONTAINER_WORK_DIR'/logs' \
+    -v $(pwd)/outputfiles:$CONTAINER_WORK_DIR'/outputfiles' \
+    -v $(pwd)/tmp:/tmp \
+    -i --rm localhost/php-cli-alpine:latest php ./run $1 $2 $3 $4 $5 $6
+
 fi
